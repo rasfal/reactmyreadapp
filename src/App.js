151f8a78af0c072a6af029dd52244c5471651731
Faizal <faizalh@win.dowjones.net>
2017-08-31 11:46:21 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
+
+import ListBooks from './ListBooks';
+import SearchBooks from './SearchBooks';
+import NewHome from './NewHome';
+import * as BooksAPI from './BooksAPI';
+import './App.css';
+
+class BooksApp extends Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    screen: 'home', //search
+    books: []
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="app">
+          <Route exact path="/" render={() => <ListBooks />} />
+          <Route path="/search" render={() => <SearchBooks />} />
+          <Route path="/new" render={() => <NewHome />} />
+        </div>
+      </Router>
+      //    {this.state.screen === 'home' &&
+      //    <ListBooks
+      //    onSearch={() => {
+      //    this.setState({ screen: 'search' });
+      //    }}
+      //    />}
+      //  {this.state.screen === 'search' && <SearchBooks />}
+    );
+  }
+}
+
+//export default BooksApp;
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(BooksApp)
+);
---
+++ src/NewHome.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+class NewHome extends Component {
+  render() {
+    return <div> In the new</div>;
+  }
+}
+
+export default NewHome;
