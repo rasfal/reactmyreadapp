ba75339c839999fe20025c7544d39f452fde921b
raseena <raseena@gmail.com>
2017-09-06 14:36:22 -0400

    search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
-import NewHome from './NewHome';
 import * as BooksAPI from './BooksAPI';
 import './App.css';
-import * as ContactAPI from './ContactAPI';
 
 class BooksApp extends Component {
   state = {
@@ -17,7 +15,57 @@ class BooksApp extends Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     //  screen: 'home', //search
-    books: []
+    books: [],
+    curReadBooks: [
+      {
+        bgImage:
+          'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api',
+        title: 'To Kill a Mockingbird',
+        authors: 'Harper Lee'
+      },
+      {
+        bgImage:
+          'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api',
+        title: 'Enders Game',
+        authors: 'Orson Scott Card'
+      }
+    ],
+
+    wantToRead: [
+      {
+        bgImage:
+          'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api',
+        title: '1776',
+        authors: 'David McCullough'
+      },
+      {
+        bgImage:
+          'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api',
+        title: 'Harry Potter and the Sorcerers Stone',
+        authors: 'J.K. Rowling'
+      }
+    ],
+
+    read: [
+      {
+        bgImage:
+          'http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api',
+        title: 'The Hobbit',
+        authors: 'J.R.R. Tolkien'
+      },
+      {
+        bgImage:
+          'http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api',
+        title: 'Oh, the Places Youll Go!',
+        authors: 'Seuss'
+      },
+      {
+        bgImage:
+          'http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api',
+        title: 'The Adventures of Tom Sawyer',
+        authors: 'Mark Twain'
+      }
+    ]
   };
 
   componentDidMount() {
@@ -30,7 +78,16 @@ class BooksApp extends Component {
     return (
       <BrowserRouter>
         <div className="app">
-          <Route exact path="/" render={() => <ListBooks />} />
+          <Route
+            exact
+            path="/"
+            render={() =>
+              <ListBooks
+                curReadBooks={this.state.curReadBooks}
+                wantToRead={this.state.wantToRead}
+                read={this.state.read}
+              />}
+          />
           <Route
             path="/search"
             render={() => <SearchBooks books={this.state.books} />}
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import escapeRegExp from 'escape-string-regexp';
+import * as BooksAPI from './BooksAPI';
 
 class SearchBooks extends Component {
   state = {
-    bookQuery: ''
+    bookQuery: '',
+    books: []
   };
 
   updateQry = bookQuery => {
     this.setState({ bookQuery: bookQuery.trim() });
   };
 
+  searchBooks(bookQuery) {
+    console.log(bookQuery);
+    BooksAPI.search(bookQuery, 20).then(books => {
+      this.setState({ books });
+    });
+  }
+
   render() {
     const { bookQuery } = this.state;
-    const { books } = this.props;
+    const { books } = this.state;
 
     let showingBooks;
 
     if (bookQuery) {
       const mtch = new RegExp(escapeRegExp(bookQuery), 'i');
+
       showingBooks = books.filter(
         cnt => mtch.test(cnt.title) || mtch.test(cnt.authors)
       );
@@ -45,49 +55,50 @@ class SearchBooks extends Component {
               type="text"
               placeholder="Search by title or author"
               value={bookQuery}
-              onChange={event => this.updateQry(event.target.value)}
+              onChange={event => this.searchBooks(event.target.value)}
             />
           </div>
         </div>
         <div className="search-books-results" />
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {showingBooks.map(book =>
-              <li key={book.title}>
-                <div className="book">
-                  <div className="book-top">
-                    <div
-                      className="book-cover"
-                      style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage:
-                          'url(' + book.imageLinks.thumbnail + ')'
-                      }}
-                    />
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="none" disabled>
-                          Move to...
-                        </option>
-                        <option value="currentlyReading">
-                          Currently Reading
-                        </option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
+            {bookQuery &&
+              showingBooks.map(book =>
+                <li key={book.title}>
+                  <div className="book">
+                    <div className="book-top">
+                      <div
+                        className="book-cover"
+                        style={{
+                          width: 128,
+                          height: 193,
+                          backgroundImage:
+                            'url(' + book.imageLinks.thumbnail + ')'
+                        }}
+                      />
+                      <div className="book-shelf-changer">
+                        <select>
+                          <option value="none" disabled>
+                            Move to...
+                          </option>
+                          <option value="currentlyReading">
+                            Currently Reading
+                          </option>
+                          <option value="wantToRead">Want to Read</option>
+                          <option value="read">Read</option>
+                          <option value="none">None</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="book-title">
+                      {' '}{book.title}
+                    </div>
+                    <div className="book-authors">
+                      {' '}{book.authors}
                     </div>
                   </div>
-                  <div className="book-title">
-                    {' '}{book.title}
-                  </div>
-                  <div className="book-authors">
-                    {' '}{book.authors}
-                  </div>
-                </div>
-              </li>
-            )}
+                </li>
+              )}
           </ol>
         </div>
       </div>
